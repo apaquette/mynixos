13d51a824a82894cc26c20ac4f1ec0b222e036bb
Alex Paquette <alexandre.d.paquette@gmail.com>
2025-11-30 12:46:23 -0400

    move home-manager files into user directory

--- user/home.nix
+++ user/home.nix
@@ -38,30 +38,6 @@
   programs = {
     home-manager.enable = true;
     fish.enable = true;
-    # vscode = {
-    #   enable = true;
-    #   package = pkgs.vscode.fhs;
-
-    #   profiles.default.enableUpdateCheck = false;
-    #   profiles.default.enableExtensionUpdateCheck = false;
-
-    #   profiles.default.extensions = with pkgs.vscode-extensions;
-    #   [
-    #     bbenoist.nix
-    #     jnoortheen.nix-ide
-    #     pkief.material-icon-theme
-    #   ];
-
-    #   profiles.default.userSettings = builtins.fromJSON '' {
-    #       "diffEditor.ignoreTrimWhitespace": false,
-    #       "editor.bracketPairColorization.enabled": true,
-    #       "extensions.autoCheckUpdates": false,
-    #       "git.autofetch": true,
-    #       "update.mode": "none",
-    #       "window.titleBarStyle": "custom",
-    #       "workbench.iconTheme": "material-icon-theme",
-    #       "git.confirmSync": false
-    #     }'';
-    # };
+    firefox.enable = true;
   };
 }
