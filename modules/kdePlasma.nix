fb1099274c8f6e13fc58032246f23a3d3dbcc19a
Alex Paquette <alexandre.d.paquette@gmail.com>
2025-12-03 12:20:02 -0400

    add kde plasma module

---
+++ modules/kdePlasma.nix
@@ -0,0 +1,21 @@
+{ 
+  config, 
+  pkgs, 
+  userSettings,
+  ...
+}:
+
+{
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+    
+  # Enable KDE Connect
+  programs.kdeconnect.enable = true;
+
+  users.users.${userSettings.username} = {
+    packages = with pkgs; [
+      kdePackages.kate
+    ];
+  };
+}
