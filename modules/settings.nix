787c6eddfb08ddce64d18b1db7fe6ffa52fe6815
Alex Paquette <alexandre.d.paquette@gmail.com>
2025-12-09 19:54:03 -0400

    moved common settings between desktop and laptop configs into its own module

---
+++ modules/settings.nix
@@ -0,0 +1,98 @@
+{ 
+  pkgs,
+  userSettings,
+  hostname,
+  timezone,
+  locale,
+  ... 
+}:
+
+{
+  # Enable Bluetooth
+    hardware.bluetooth.enable = true;
+
+    networking.hostName = hostname; # Define your hostname.
+    #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # Set your time zone.
+    time.timeZone = timezone;
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = locale;
+
+    # X11
+    services.xserver = {
+      # Enable the X11 windowing system.# You can disable this if you're only using the Wayland session.
+      enable = true;
+      
+      # Configure keymap in X11
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+    };
+
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+
+    # Enable sound with pipewire.
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.${userSettings.username} = {
+      isNormalUser = true;
+      shell = pkgs.fish;
+      description = userSettings.name;
+      extraGroups = [ "networkmanager" "wheel" ];
+    };
+
+    # Automatic updating
+    system.autoUpgrade = {
+      enable = true;
+      dates = "weekly";
+    };
+
+    environment.shells = with pkgs; [ fish ];
+    users.defaultUserShell = pkgs.fish;
+    programs.fish.enable = true;
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
+
+    # List services that you want to enable:
+
+    # Enable the OpenSSH daemon.
+    # services.openssh.enable = true;
+
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
+
+}
