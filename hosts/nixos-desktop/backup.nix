2e18180f718a67bb15a29a107edd1468e30ab472
Alex Paquette <alexandre.d.paquette@gmail.com>
2025-12-20 17:13:49 -0400

    update backup configuration

--- hosts/nixos-desktop/backup.nix
+++ hosts/nixos-desktop/backup.nix
@@ -1,4 +1,9 @@
-{ ... }:
+{ 
+  config,
+  pkgs,
+  userSettings,
+  ... 
+}:
 
 {
   # Mount backup drive
@@ -8,16 +13,25 @@
     options = [ "nofail" ];
   };
 
+  ## Borg backup service
   services.borgbackup.jobs.system-backup = {
     encryption.mode = "none";
 
     paths = [
-      "/home/apaquette/Documents"
-      "/home/apaquette/Pictures"
+      "/home/${userSettings.username}/Documents"
+      "/home/${userSettings.username}/Pictures"
       "/mnt/myraid/Audiobooks"
       "/mnt/myraid/Emulators"
-      "/mnt/myraid/Videos"
+      #"/mnt/myraid/Videos"
     ];
+
+    # patterns = [
+    #   "+ /mnt/myraid/Videos/**/*.avi"
+    #   "+ /mnt/myraid/Videos/**/*.mkv"
+    #   "+ /mnt/myraid/Videos/**/*.mp4"
+
+    #   "- /mnt/myraid/Videos/**"
+    # ];
 
     repo = "/mnt/backup/borg";
     compression = "auto,zstd";
@@ -29,4 +43,49 @@
       monthly = 6;
     };
   };
+
+  # rsync backup service
+  # systemd.services.media-backup = {
+  #   after = [ "mnt-backup.mount" ];
+  #   requires = [ "mnt-backup.mount" ];
+
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     User = "root";
+
+  #     ExecStart = ''
+  #       ${pkgs.rsync}/bin/rsync -a \
+  #         --delete \
+  #         --prune-empty-dirs \
+  #         --numeric-ids \
+  #         --info=stats2 \
+  #         --include='*/' \
+  #         --include='*.mkv' \
+  #         --include='*.mp4' \
+  #         --include='*.avi' \
+  #         --include='*.mov' \
+  #         --include='*.m4v' \
+  #         --include='*.webm' \
+  #         --include='*.srt' \
+  #         --include='*.ass' \
+  #         --include='*.ssa' \
+  #         --include='*.sub' \
+  #         --include='*.vtt' \
+  #         --include='*.idx' \
+  #         --include='*.sup' \
+  #         --exclude='*' \
+  #         /mnt/myraid/Videos/ \
+  #         /mnt/backup/Videos/
+  #     '';
+  #   };
+  # };
+
+  # systemd.timers.media-backup = {
+  #   wantedBy = [ "timers.target" ];
+
+  #   timerConfig = {
+  #     OnCalendar = "daily";
+  #     Persistent = true;
+  #   };
+  # };
 }
